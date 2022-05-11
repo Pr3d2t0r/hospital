92fb40d2a3f5bd86cd858d82c4e244d260d2d61e
Velosa <rafaelvelosa18@gmail.com>
2022-05-11 11:50:13 +0100

    medicos add

--- application/controllers/Medicos.php
+++ application/controllers/Medicos.php
@@ -27,7 +27,7 @@ class Medicos extends MY_Controller
 
     public function add(){
         $data = [
-            "title" => "Add Medic"
+            "title" => "Add Doctor"
         ];
 
         $this->form_validation->set_rules('name', 'Name', 'required|min_length[3]|max_length[80]');
@@ -37,15 +37,23 @@ class Medicos extends MY_Controller
         $this->form_validation->set_rules('address', 'Address', 'required|min_length[5]');
         $this->form_validation->set_rules('city', 'City', 'required|min_length[5]|max_length[100]');
         $this->form_validation->set_rules('birthday', 'Birthdate', 'required');
-        $this->form_validation->set_rules('image', 'Image', 'required');
+//        $this->form_validation->set_rules('image', 'Image', 'required');
 
         if (!$this->form_validation->run()) {
             //validation_errors() -> método responsável por recuperar as mensagens
             $data['formErrors'] = validation_errors();
         } else {
             $data['formErrors'] = null;
-            $this->session->set_flashdata("success_msg", "Contact sent with success!");
-            $this->MedicosModel->insert($this->input->post());
+            $this->session->set_flashdata("success_msg", "Doctor added with success!");
+            $result_arr = $this->input->post();
+            $address_arr = [
+                "name" => $result_arr['address'],
+                "city" => $result_arr['city']
+            ];
+            unset($result_arr['address'], $result_arr['city']);
+            var_dump($result_arr);
+            var_dump($address_arr);
+            //$this->MedicosModel->insert($result_arr);
         }
 
         $this->load->view('commons/header', $data);
--- application/views/medicos_add.php
+++ application/views/medicos_add.php
@@ -1,7 +1,7 @@
 <div class="container">
     <h1>Add Doctor</h1>
     <div id="addMedico">
-        <form action="<?php echo base_url("doctors/add"); ?>" method="post">
+        <form action="<?php echo base_url("doctors/add"); ?>" method="post" enctype="multipart/form-data">
             <input type="text" name="name" placeholder="Name"><br>
             <input type="text" name="nib" placeholder="Nib"><br>
             <input type="text" name="nif" placeholder="Nif"><br>
@@ -10,7 +10,8 @@
             <input type="text" name="city" placeholder="City"><br>
             <label for="">Birthdate: <input type="date" name="birthday" placeholder="Birthday"></label><br>
             <label for="">Image: <input type="file" name="image" placeholder="Image"></label><br>
-            <input type="submit" name="Store">
+            <input type="submit" value="Add">
         </form>
     </div>
+    <?php echo $formErrors; ?>
 </div>
